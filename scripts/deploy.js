43a3e1f71011a93b53169897810487b7bbfa4b3b
Hira Siddiqui <hisi@mms-t-systems.de>
2023-04-10 22:26:25 +0200

    SemaphoreIdentity contract and deployment file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,7 @@
 async function main() {
+    //setup variables
+    const semaphore_verifier_address = "0x3d3df6CFc6BFf68d9693e097F32bF4a9903E77a5"; //sepolia address for semaphore verifier contract
+    const incremental_binary_tree_address = "0x181B7f34538cE3BceC68597d4A212aB3f7881648"; //sepolia address for IncrementalBinaryTree library
 
     const [deployer] = await ethers.getSigners();
 
@@ -9,8 +12,13 @@ async function main() {
 
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
-    const SemaphoreIdentity = await ethers.getContractFactory("SemaphoreIdentity");
-    const contract = await SemaphoreIdentity.deploy();
+    const SemaphoreIdentity = await ethers.getContractFactory("SemaphoreIdentity", {
+      libraries: {
+        IncrementalBinaryTree : incremental_binary_tree_address 
+      }
+    });
+
+    const contract = await SemaphoreIdentity.deploy(semaphore_verifier_address);
 
     console.log("Contract deployed at:", contract.address);
 }
