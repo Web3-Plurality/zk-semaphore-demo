d0482ea021276820963d09b9c9a7a9063f50b998
Hira Siddiqui <hisi@mms-t-systems.de>
2023-04-12 22:16:31 +0200

    Adding layout to verifier and dapp pages

--- src/pages/DApp.js
+++ src/pages/DApp.js
@@ -1,11 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../bootstrap.css';
+import { createGroup, addMemberToGroup, verifyMemberIsPartOfGroup, removeMemberFromGroup } from '../components/Web3Client';
+import verifiedImg from '../images/verified.png';
+import unverifiedImg from '../images/unverified.png';
 
+
+let identity;
+let verifyRequestDappTx;
 
 const DApp = () => {
+
+  const [isVerifyRequestDAppDisabled, setVerifyRequestDAppDisabled] = useState(false);
+  const [isCheckVerificationStatusDisabled, setCheckVerificationStatusDisabled] = useState(true);
+  const [textAreaValue, setTextAreaValue] = useState('');
+
+  const imagRef=React.createRef();
+  const imageRef=useRef();
+
+
+  async function verifyRequestDApp() {
+    imageRef.current.src = verifiedImg;
+
+    alert('User is requesting the DApp for Verification!');
+    setVerifyRequestDAppDisabled(true);
+
+    // generate proof
+    const identityString = localStorage.getItem("identity");
+    verifyMemberIsPartOfGroup(identity).then(tx => {
+      console.log(tx);
+      console.log(tx.events.ProofVerified);
+      verifyRequestDappTx = tx;
+      setTextAreaValue(`Your zero knowledge proof is being checked against the contract`);
+      setCheckVerificationStatusDisabled(false);
+
+    }).catch(err => {
+      console.log(err);
+      verifyRequestDappTx = err;
+      setTextAreaValue(`Your zero knowledge proof is invalid. Access denied`);
+    }); 
+
+  }
+  
+  function checkVerificationStatus() {
+    alert('Check Verification Status at DApp Contract!');
+    setCheckVerificationStatusDisabled(true);
+    console.log(verifyRequestDappTx)
+    try {
+      if (verifyRequestDappTx.events.ProofVerified)
+        setTextAreaValue('Access Granted. Your proof was valid. \n');
+    }
+    catch(err) {
+      setTextAreaValue('Access Denied. You are not a part of valid participants \n');
+    }
+  }
+
     return (
-        <div>
-          <h1>DApp</h1>
+        <div class="text-center">
+          <h1 class="display-4 text-center">Mortgage Loans DApp</h1>
+          <br />
+          <h4>Already verified yourself through our verifier? Please provide zero knowledge proof of verification</h4>
+          <button onClick={verifyRequestDApp} type="button" class="btn btn-primary me-md-2" disabled={isVerifyRequestDAppDisabled} data-bs-toggle="button" autocomplete="off">Request DApp for membership verification</button>
+          <button onClick={checkVerificationStatus} type="button" class="btn btn-primary me-md-2" disabled={isCheckVerificationStatusDisabled} data-bs-toggle="button" autocomplete="off">Check Verification Status at DApp</button>
+          <br/>
+
+          <img src={unverifiedImg} ref={imageRef} style={{width: '300px', height: '300px'}} />
+
+          <div class="mb-3" id="textarea-readonly">
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="12" value={textAreaValue} aria-label="Disabled input example" disabled readonly></textarea>
+          </div>
         </div>
       );
 }
--- src/pages/Verifier.js
+++ src/pages/Verifier.js
@@ -12,14 +12,35 @@ const Verifier = () => {
 
 
   function generateQR() {
-    //alert('Creating a new proof invitation');
     const min = 1;
     const max = 10000;
     const rand = min + Math.floor(Math.random() * 10) * (max - min);
-    setText(`Random invitation string # ${rand}`);
+
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=1')
+    .then(response => response.json())
+    .then(data => {
+
+      console.log(JSON.stringify(data));
+      setText(`${JSON.stringify(data)} # ${rand}`)
+      setTextAreaValue("Scan the QR code from your mobile app to initiate the connection")
+    });
+  }
+
+  function verifyProof() {
+    // receive proof from mobile app
+    // setTextAreaValue("Proof from mobile app has been received")
+    // verify proof post call
+    // get result from post call
+    // setTextAreaValue("Proof has been verified")
+    // get did from proof
+    // create identity
+    // localStorage.setItem("identity", identity);
+    // create group
+    // add member to group
   }
 
   useEffect(() => {
+
     QRCode.toCanvas(
       canvasRef.current,
       // QR code doesn't work with an empty string
@@ -32,22 +53,24 @@ const Verifier = () => {
         canvasRef.current.style.height = `500px`
       },
     );
-    generateQR();
+    //generateQR();
   }, [text]);
 
 
     return (
         <div class="text-center">
-          <h1 class="display-4 text-center">Verifier</h1>
+          <h1 class="display-6 text-center">Verifier for Mortgage Loans DApp</h1>
           <br/>
-          <h4 class="display-6 text-center">Scan the QR code to connect to verifier</h4>
+          <h4 class="text-center">Scan the QR code to connect to verifier and provide proof details</h4>
           <br/>
-          <p>{text}</p>
+          <p><b>Proof Required: Name & Address from Identity Card & Credit Rating>8 from Financial History </b> </p>
           <div style={{ display: 'flex', alignItems: 'center', justifyContent: "center"}}>
             
             <br />
             <canvas ref={canvasRef}  />
           </div>
+          <p>Connection Url: <br/>{text}</p>
+          
           <button onClick={generateQR} type="button" class="btn btn-primary me-md-2" data-bs-toggle="button">Generate Proof Invitation</button>
           <br/> <br/> <br/>
           <textarea class="form-control" id="exampleFormControlTextarea1" rows="12" value={textAreaValue} aria-label="Disabled input example" disabled readonly></textarea>
