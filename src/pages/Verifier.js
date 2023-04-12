abb9309001021d9a4bdf7746ac2a41cf8e1f76ef
Hira Siddiqui <hisi@mms-t-systems.de>
2023-04-13 00:23:07 +0200

    Added SSI connection and proof request calls in the verifier page

--- src/pages/Verifier.js
+++ src/pages/Verifier.js
@@ -4,6 +4,9 @@ import { Identity } from "@semaphore-protocol/identity";
 import { createGroup, addMemberToGroup, verifyMemberIsPartOfGroup, removeMemberFromGroup } from '../components/Web3Client';
 import QRCode from "qrcode";
 import mortgage from '../images/mortgage.png';
+import axios from 'axios'; 
+import { encode } from "base-64";
+
 
 const Verifier = () => {
   const [text, setText] = useState("");
@@ -13,19 +16,108 @@ const Verifier = () => {
   const logRef=React.createRef();
   const logoRef=useRef();
 
+  let partnerId;
+  let connectionId;
+  const proofTemplateId = "ecfacbf5-c75b-4867-bcf6-9258ede36525";
   function generateQR() {
-    const min = 1;
-    const max = 10000;
-    const rand = min + Math.floor(Math.random() * 10) * (max - min);
 
-    fetch('https://jsonplaceholder.typicode.com/posts?_limit=1')
-    .then(response => response.json())
-    .then(data => {
+    axios.post('http://bpa.westeurope.cloudapp.azure.com:8080/api/invitations', {}, {
+      auth: {
+        username: 'admin',
+        password: 'changeme'
+      }
+    }).then(response => {
+        console.log(response);
+        const invitationUrl = response.data.invitationUrl;
+        console.log(invitationUrl);
+        setText(invitationUrl);
+        partnerId = response.data.partnerId;
+        connectionId = response.data.connectionId;
 
-      console.log(JSON.stringify(data));
-      setText(`${JSON.stringify(data)} # ${rand}`)
-      setTextAreaValue("Scan the QR code from your mobile app to initiate the connection")
+        setTextAreaValue(`Step 1/4 Started: Created connection invitation for the user. \n`);
+        // start looping
+        waitForAcceptance();
+    })
+    .catch(error => {
+      console.error('There was an error!', error);
     });
+  }
+
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async function waitForAcceptance() {
+    let status;
+    const url = `http://bpa.westeurope.cloudapp.azure.com:8080/api/partners/${partnerId}`;
+    console.log(`Wait for invitation acceptance url: ${url}`);
+    while (status != "response")
+    {
+      fetch(url, {
+        method:'GET', 
+        headers:{'Authorization': 'Basic ' + encode('admin:changeme')}})
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        status = json.state;
+        console.log(status);
+        if (status === "response")
+        {
+          console.log(`connection id is: ${connectionId}`);
+          setTextAreaValue(`Step 1/4 Complete: Connection Invitation has been accepted by user. \n
+                            Step 2/4 Started: Sending proof request to user.`);
+          requestProofPresentation();
+        }
+      }); 
+      await sleep(5000);
+    }
+  }
+
+  function requestProofPresentation() {
+    const url = `http://bpa.westeurope.cloudapp.azure.com:8080/api/partners/${partnerId}/proof-request/${proofTemplateId}`;
+    console.log(`Request proof presentation url: ${url}`)
+    axios.put(url, {}, {
+      auth: {
+        username: 'admin',
+        password: 'changeme'
+      }
+    }).then(response => {
+        console.log(response);
+        setTextAreaValue(`Step 2/4 Waiting: Proof Request Sent. Waiting for user's proof presentation.`);
+        // start looping
+        waitForProofPresentation();
+    })
+    .catch(error => {
+      console.error('There was an error!', error);
+    });
+  }
+
+  async function waitForProofPresentation() {
+    let status;
+    let proofData;
+    const url = `http://bpa.westeurope.cloudapp.azure.com:8080/api/partners/${partnerId}/proof-exchanges/${proofTemplateId}`;
+    console.log(url);
+    while (status != "verified")
+    {
+      fetch(url, {
+        method:'GET', 
+        headers:{'Authorization': 'Basic ' + encode('admin:changeme')}})
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        status = json.state;
+        console.log(status);
+        if (status === "verified")
+        {
+          proofData = json.proofData;
+          setTextAreaValue(`Step 2/4 Complete: Proof Request Received and Verified. \n
+                            User's revealed proof data is: \n
+                            ${JSON.stringify(proofData)}\n
+                            Step 3/4 Started: Creating zero-knowledge identity for this user`);
+        }
+      }); 
+      await sleep(5000);
+    }
   }
 
   function verifyProof() {
