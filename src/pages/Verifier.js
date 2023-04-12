c81899b49ef1f408092de51a4571c6c00bf8cb6e
Hira Siddiqui <hisi@mms-t-systems.de>
2023-04-12 19:04:37 +0200

    styled verifier page

--- src/pages/Verifier.js
+++ src/pages/Verifier.js
@@ -8,6 +8,16 @@ import QRCode from "qrcode";
 const Verifier = () => {
   const [text, setText] = useState("");
   const canvasRef = useRef();
+  const [textAreaValue, setTextAreaValue] = useState("Results");
+
+
+  function generateQR() {
+    //alert('Creating a new proof invitation');
+    const min = 1;
+    const max = 10000;
+    const rand = min + Math.floor(Math.random() * 10) * (max - min);
+    setText(`Random invitation string # ${rand}`);
+  }
 
   useEffect(() => {
     QRCode.toCanvas(
@@ -15,24 +25,33 @@ const Verifier = () => {
       // QR code doesn't work with an empty string
       // so we are using a blank space as a fallback
       text || " ",
-      (error) => error && console.error(error)
+      (error) => error && console.error(error),
+      () => {
+        // fix again the CSS because lib changes it –_–
+        canvasRef.current.style.width = `500px`
+        canvasRef.current.style.height = `500px`
+      },
     );
+    generateQR();
   }, [text]);
 
 
     return (
-        <div>
-          <h1>Verifier</h1>
-          <div>
-            <input
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-            />
+        <div class="text-center">
+          <h1 class="display-4 text-center">Verifier</h1>
+          <br/>
+          <h4 class="display-6 text-center">Scan the QR code to connect to verifier</h4>
+          <br/>
+          <p>{text}</p>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: "center"}}>
+            
             <br />
-            <canvas ref={canvasRef} />
+            <canvas ref={canvasRef}  />
           </div>
+          <button onClick={generateQR} type="button" class="btn btn-primary me-md-2" data-bs-toggle="button">Generate Proof Invitation</button>
+          <br/> <br/> <br/>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="12" value={textAreaValue} aria-label="Disabled input example" disabled readonly></textarea>
         </div>
-
       );
 }
 
